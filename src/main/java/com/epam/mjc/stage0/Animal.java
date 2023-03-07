f285adaccba50fd34dd10d2e79f92bdd96313477
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-07 15:47:12 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,9 +1,9 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-     private String color;
-    private int numberOfPaws;
-    private boolean hasFur;
+    public String color;
+    public int numberOfPaws;
+    public boolean hasFur;
 
     public Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
