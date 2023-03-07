daf1b2ea20eb4c305690a9482794ced55933041c
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-07 15:14:39 +0600

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -4,11 +4,10 @@ public class Bird {
     public Bird() {
         super("blue", 2, false);
     }
-    
     @Override
     public String getDescription() {
         String plural = getNumberOfPaws() == 1 ? "paw" : "paws";
-        String fur = hasFur ? "a" : "no";
+        String fur = isHasFur() ? "a" : "no";
         return super.getDescription() + " Moreover, it has 2 wings and can fly.";
     }
 }
