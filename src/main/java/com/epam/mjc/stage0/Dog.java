79e3be363b86c33850db56aea85ea286fd459667
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-07 15:30:17 +0600

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -4,13 +4,4 @@ public class Dog {
   public Dog() {
         super("brown", 4, true);
     }
-  public static void main(String[] args) {
-        Animal animal = new Animal("black", 3, true);
-        Dog dog = new Dog();
-        Bird bird = new Bird();
-
-        System.out.println(animal.getDescription());
-        System.out.println(dog.getDescription());
-        System.out.println(bird.getDescription());
-    }
 }
