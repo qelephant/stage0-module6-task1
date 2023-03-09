196ad950c48214256933a1aa9da88c0b36e73144
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-09 11:15:38 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -19,17 +19,7 @@ public class Animal {
         String fur = hasFur ? "a" : "no";
         return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + plural + " and " + fur + " fur.";
     }
-
-    public static void main(String[] args) {
-        Animal animal = new Animal("blue", 1, false);
-        Dog dog = new Dog();
-        Bird bird = new Bird();
-
-        System.out.println(animal.getDescription());
-        System.out.println(dog.getDescription());
-        System.out.println(bird.getDescription());
-    }
-
+    
     public String getColor() {
         return color;
     }
