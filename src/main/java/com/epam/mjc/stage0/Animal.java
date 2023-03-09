75daea53a27c251a1e91be36c894432f8a5f89cc
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-09 11:14:14 +0600

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,9 +1,9 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-    public String color;
-    public int numberOfPaws;
-    public boolean hasFur;
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
     public Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
@@ -11,10 +11,36 @@ public class Animal {
         this.hasFur = hasFur;
     }
 
+    public Animal() {
+    }
+
     public String getDescription() {
         String plural = numberOfPaws == 1 ? "paw" : "paws";
         String fur = hasFur ? "a" : "no";
         return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + plural + " and " + fur + " fur.";
     }
-   
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("blue", 1, false);
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+
+        System.out.println(animal.getDescription());
+        System.out.println(dog.getDescription());
+        System.out.println(bird.getDescription());
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
 }
+
+
